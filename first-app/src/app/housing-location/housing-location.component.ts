1ca969f81416833c8cafb195bc80ccb15ca807cd
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-01 16:14:05 +0200

    routing progress

--- first-app/src/app/housing-location/housing-location.component.ts
+++ first-app/src/app/housing-location/housing-location.component.ts
@@ -1,10 +1,12 @@
 import { Component, Input } from '@angular/core';
 import { Housinglocation } from '../housinglocation';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { RouterOutlet } from '@angular/router';
 @Component({
   selector: 'app-housing-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './housing-location.component.html',
   styleUrl: './housing-location.component.css'
 })
