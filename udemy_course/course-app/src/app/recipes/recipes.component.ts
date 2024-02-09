464f95a5358b81192f760d8ddaef5b6926ce29ee
sixLUKEY <marcoslukerojo@gmail.com>
2024-02-09 15:39:34 +0200

    components linked

--- udemy_course/course-app/src/app/recipes/recipes.component.ts
+++ udemy_course/course-app/src/app/recipes/recipes.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
 
 @Component({
   selector: 'app-recipes',
   standalone: true,
-  imports: [],
+  imports: [RecipeDetailComponent, RecipeListComponent],
   templateUrl: './recipes.component.html',
   styleUrl: './recipes.component.css'
 })
